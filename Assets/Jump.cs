a3f6c3773dafd862c3955629e4e8c2d9feb9fe57
Yonman2002 <40135047+Yonman2002@users.noreply.github.com>
2019-10-05 12:40:07 +0300

    Did stuff

---
+++ Assets/Jump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpSpeed;
+    public CheckJumpable jumpCollider;
+    public Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Vertical") && jumpCollider.isAbleToJump)
+        {
+            ActivateJump();
+        }
+    }
+
+    public void ActivateJump()
+    {
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpSpeed);
+    }
+}
